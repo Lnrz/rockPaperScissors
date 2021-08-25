a670862506efbe75cbf465f4710541b7d4dfe69e
Lorenzo <loreevil@gmail.com>
2021-08-25 16:04:21 +0200

    Finita interfaccia grafica, iniziato parte in JS

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@
 function computerSelection() {
     return Math.floor(Math.random() * 3);
 }
-
+/*
 function convertIntoString(n) {
 
     switch (n) {
@@ -20,75 +20,126 @@ function convertIntoString(n) {
             return "Forbici";
     }
 }
-
+*/
 function convertIntoNum(playerSelection) {
-
-    playerSelection = playerSelection.toLowerCase().trim();
 
     switch (playerSelection) {
 
-        case "sasso":
+        case "rock":
             return 0;
-        case "carta":
+        case "paper":
             return 1;
-        case "forbici":
+        case "scissors":
             return 2;
-        default:
-            return -1;
     }
 }
 
-function computerPlay(playerSelection, computerSelection) {
+function isPlayerVictory(computerSelection, playerSelection) {
     
     if (playerSelection == computerSelection) {
-        return `Pareggio!`;
+        return 0;
 
     } else if (playerSelection == (computerSelection - 1) % 3) {
-        return `Sconfitta! ${convertIntoString(computerSelection)} vince su ${convertIntoString(playerSelection)}`;
+        return -1;
 
     } else {
-        return `Vittoria! ${convertIntoString(playerSelection)} vince su ${convertIntoString(computerSelection)}`;
+        return 1;
     }
 }
 
-/*
-function game() {
+const imagesList = [...document.querySelectorAll("#chooseImages > img")];
+const button = document.querySelector("button");
 
-    let playerSelection = -1;
-    let playerWins = 0;
-    let computerWins = 0;
-    let result;
+function selectUnselect(obj) {
+    
+    let slcImg = obj.target;
 
-    for (let i = 0; i < 5; i++, playerSelection = -1) {
+    imagesList.forEach(img => img.classList.remove("selected"));
 
-        while (playerSelection < 0) {
-            
-            playerSelection = prompt("Sasso, carta o forbici?");
-
-            playerSelection = convertIntoNum(playerSelection);
-
-            if(playerSelection == -1) console.log(`Inserire un valore valido`);
-        }
-
-        result = computerPlay(playerSelection, computerSelection());
-
-        console.log(result);
-
-        result = result.charAt(0);
-
-        if (result == `V`) {
-            playerWins++;
-        } else if (result == `S`) {
-            computerWins++;
-        }
-    }
-
-    if (playerWins > computerWins) {
-        console.log(`Hai vinto con ${playerWins} punti!`);
-    } else if (playerWins < computerWins) {
-        console.log(`Ha vinto il computer con ${computerWins} punti!`);
-    } else {
-        console.log(`Pareggio con ${playerWins} punti a testa!`);
-    }
+    slcImg.classList.add("selected");
 }
-*/
+
+function addComputerChoose(n) {
+
+    const computerChoose = document.querySelector("#computerChoose");
+    const img = document.createElement("img");
+
+    switch (n) {
+        case 0:
+            img.setAttribute("src", "images/rock.png");
+            break;
+        case 1:
+            img.setAttribute("src", "images/paper.png");
+            break;
+        case 2:
+            img.setAttribute("src", "images/scissors.png");
+            break;
+    }
+
+    computerChoose.appendChild(img);
+}
+
+function addParagraph(n) {
+
+    const div = document.querySelector("#result");
+    const p = document.createElement("p");
+
+    switch (n) {
+        case -1:
+            p.textContent = "Hai perso!";
+            break;
+        case 0:
+            p.textContent = "Pareggio!";
+            break;
+        case 1:
+            p.textContent = "Hai vinto!";
+            break;
+    }
+
+    div.appendChild(p);
+}
+
+function addPoint(n) {
+
+    if (!n) return;
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "images/point.png");
+
+    const div = document.querySelector(
+        (n > 0) ? "#playerVictories" :
+        "#computerVictories"
+    );
+
+    div.appendChild(img);
+}
+
+function playGame() {
+
+    let slcImg = imagesList.filter(img => {
+        return img.classList.value.includes("selected");
+    });
+
+    slcImg = slcImg[0];
+
+    if (!slcImg) {
+        alert("Selezionare una delle icone per giocare")
+        return;
+    }
+
+    let pcChoose = computerSelection();
+
+    addComputerChoose(pcChoose);
+
+    let result = isPlayerVictory( pcChoose,
+        convertIntoNum(slcImg.getAttribute("id"))
+    );
+
+    addParagraph(result);
+
+    addPoint(result);
+}
+
+imagesList.forEach(img => img.addEventListener("click", selectUnselect));
+
+button.addEventListener("click", playGame)
